4f795ca217814dc0a1f6d667fe8e2cd5e5f5d2af
abusayeid11 <abusayeidsawon@gmail.com>
2026-04-26 18:31:28 +0600

    fix: remove duplicate CORS config, use global CorsConfig only

--- backend-api/src/main/java/com/ytmanager/controller/TranscriptController.java
+++ backend-api/src/main/java/com/ytmanager/controller/TranscriptController.java
@@ -7,7 +7,6 @@ import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +20,6 @@ import com.ytmanager.service.TranscriptService;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:3000")
 public class TranscriptController {
 
     private static final Logger logger = LoggerFactory.getLogger(TranscriptController.class);
